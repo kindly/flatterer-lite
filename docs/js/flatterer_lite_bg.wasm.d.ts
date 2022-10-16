8cf5d2111d298187872d595292b3662a4ee3c985
David Raznick <kindly@gmail.com>
2022-10-16 02:16:12 +0100

    add js

---
+++ docs/js/flatterer_lite_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function set_logger(): void;
+export function get_url(a: number, b: number, c: number): number;
+export function from_string(a: number, b: number, c: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9b7c48c8c0c95c22(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__hb85c522e197492cf(a: number, b: number, c: number, d: number): void;
